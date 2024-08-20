26253e5d3d3a4ade0de49d2380cd7c4ddc0521dd
isy <113178393+iswbela@users.noreply.github.com>
2024-08-19 23:00:13 -0300

    done creating table and api endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,57 @@
-export const signup = (req,res) =>{
-    console.log(signupUser)
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ error: "Passwords dont match" })
+        }
+
+        const user = await User.findOne({ username });
+
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password:hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+    if(newUser){
+        await newUser.save();
+
+        console.log("New user saved")
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        });
+    }else{
+        res.status(400).json({error:"Invalid user data"})
+    }
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: "Internal Server Error" })
+    }
 }
-export const login = (req,res) =>{
-    console.log(loginUser)
+export const login = (req, res) => {
+    console.log("loginUser")
 }
-export const logout = (req,res) =>{
-    console.log(loginUser)
+export const logout = (req, res) => {
+    console.log("logout")
 }
