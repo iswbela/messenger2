50f23d07ce59cc0fac2045461e66851213955c6e
isy <113178393+iswbela@users.noreply.github.com>
2024-08-22 21:50:44 -0300

    message endpoint done

---
+++ server/middleware/protectRoute.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if(!token){
+            return res.status(401).json({error: "Unauthorized access"});  
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if(!decoded){
+            return res.status(401).json({error: "Unauthorized access"});  
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user){
+            return res.status(401).json({error: "Unauthorized access"});  
+        }
+
+        req.user = user;
+
+        next();
+
+    } catch (error) {
+        console.log("protect route", error.message);
+        error.status(500).json({error: "Internal Server Error"});
+    }
+};
+
+export default protectRoute;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import express from 'express'; 
 import dotenv from 'dotenv';
-
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from './db/connectToMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 5000
 
 dotenv.config();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get('/', (req, res) => {
 //   res.send('Hello world');
