51f5e3920a4a751caea35fc88816db50a328e825
isy <113178393+iswbela@users.noreply.github.com>
2024-08-26 22:37:19 -0300

    Messages and user controllers/routes done

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -36,3 +36,23 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id:userToChatId}= req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: {$all: [senderId, userToChatId]},
+        }).populate("messages");
+
+        if(!conversation) return res.status(200).json([]);
+        
+        const messages = conversation.messages;
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("getMessages controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 import express from 'express'; 
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.routes.js";
+
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
 
@@ -12,13 +15,10 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
-// app.get('/', (req, res) => {
-//   res.send('Hello world');
-// });
-
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
